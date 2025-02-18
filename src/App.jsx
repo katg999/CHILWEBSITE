4f1ad6aeab72164fe095471493fbf99e9102cc20
braingilbert <briangilbert@DESKTOP-BOD1Q8N.localdomain>
2025-02-18 18:07:46 +0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/NavBar";
+import { useState } from "react";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>Welcome to My React + Vite Website</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <h1>My Website</h1>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/services">Services</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
