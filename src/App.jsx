da1aaa9666ab23bdc783edd75da8c0a0908336b5
braingilbert <briangilbert@DESKTOP-BOD1Q8N.localdomain>
2025-03-04 18:43:53 +0300

    CorrectedError

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from "./components/NavBar";
 import Display from "./components/Display";
 import ImageDisplay from "./components/ImageDisplay";
 import BenefitsSection from "./components/BenefitsSection";
+import KetiStats from "./components/KetiStats";
 
 const App = () => {
   return (
@@ -19,6 +20,7 @@ const App = () => {
         {/* Image Display Component */}
         <ImageDisplay />
         <BenefitsSection />
+        <KetiStats />
       </div>
     </Router>
   );
