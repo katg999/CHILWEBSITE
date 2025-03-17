95e850fac39d53347abb34216df4c5a589f8d15d
braingilbert <briangilbert@DESKTOP-BOD1Q8N.localdomain>
2025-03-17 16:12:58 +0300

    ResponsivenessBranchUpdate

--- src/App.jsx
+++ src/App.jsx
@@ -12,49 +12,58 @@ import News from "./components/News";
 import ActNow from "./components/ActNow";
 import OurWorksSection from "./components/OurWorksSection";
 import Footer from "./components/Footer";
-import ContactUs from "./components/ContactUs"; // Import the ContactUs component
+import ContactUs from "./components/ContactUs";
 import FinanceLoans from "./components/FinanceLoans";
 import WhatWeTreat from "./components/WhatWeTreat";
 import Disaster from "./components/Disaster";
 import PrivacyPolicy from "./components/PrivacyPolicy";
 
+// Material-UI Components
+import { Container, Box } from "@mui/material";
+
 const App = () => {
   return (
     <Router>
-      <div className="min-h-screen">
+      <Box
+        sx={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
+      >
         {/* Navbar */}
         <Navbar />
 
-        {/* Routes */}
-        <Routes>
-          {/* Home Route */}
-          <Route
-            path="/"
-            element={
-              <>
-                <Display />
-                <BenefitsSection />
-                <KetiStats />
-                <OurWorksSection />
-                <KetiServices />
-                <Testimonials />
-                <News />
-                <ActNow />
-              </>
-            }
-          />
+        {/* Main Content */}
+        <Box component="main" sx={{ flexGrow: 1 }}>
+          <Container maxWidth="xl">
+            <Routes>
+              {/* Home Route */}
+              <Route
+                path="/"
+                element={
+                  <>
+                    <Display />
+                    <BenefitsSection />
+                    <KetiStats />
+                    <OurWorksSection />
+                    <KetiServices />
+                    <Testimonials />
+                    <News />
+                    <ActNow />
+                  </>
+                }
+              />
 
-          {/* Contact Us Route */}
-          <Route path="/contact-us" element={<ContactUs />} />
-          <Route path="/asset-finance-loans" element={<FinanceLoans />} />
-          <Route path="/what-we-treat" element={<WhatWeTreat />} />
-          <Route path="/disaster" element={<Disaster />} />
-          <Route path="/privacypolicy" element={<PrivacyPolicy />} />
-        </Routes>
+              {/* Other Routes */}
+              <Route path="/contact-us" element={<ContactUs />} />
+              <Route path="/asset-finance-loans" element={<FinanceLoans />} />
+              <Route path="/what-we-treat" element={<WhatWeTreat />} />
+              <Route path="/disaster" element={<Disaster />} />
+              <Route path="/privacypolicy" element={<PrivacyPolicy />} />
+            </Routes>
+          </Container>
+        </Box>
 
         {/* Footer */}
         <Footer />
-      </div>
+      </Box>
     </Router>
   );
 };
